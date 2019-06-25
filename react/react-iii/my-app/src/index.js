0f7fcb8061964706fabc4d2989f292a6a1d22b88
robertacintra <roo.moony@gmail.com>
2019-06-25 16:42:11 -0300

    semana três - react

--- react/react-iii/my-app/src/index.js
+++ react/react-iii/my-app/src/index.js
@@ -3,38 +3,67 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-let count = 0;
+// let count = 0;
 
-class Contador extends React.Component {
-    addUm = () => {
-        count++
-        console.log(count)
-        ReactDOM.render(< Contador />, document.getElementById('root'));
-    }
-    subtrairUm = () => {
-        count--
-        console.log(count)
-        ReactDOM.render(< Contador />, document.getElementById('root'));
-    }
-    resetAll = () => {
-        count = 0
-        console.log(count)
-        ReactDOM.render(< Contador />, document.getElementById('root'));
+// class Contador extends React.Component {
+//     addUm = () => {
+//         count++
+//         console.log(count)
+//         ReactDOM.render(< Contador />, document.getElementById('root'));
+//     }
+//     subtrairUm = () => {
+//         count--
+//         console.log(count)
+//         ReactDOM.render(< Contador />, document.getElementById('root'));
+//     }
+//     resetAll = () => {
+//         count = 0
+//         console.log(count)
+//         ReactDOM.render(< Contador />, document.getElementById('root'));
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h2>Count: {count}</h2>
+//                 <button onClick={this.addUm}>+1</button>
+//                 <button onClick={this.subtrairUm}>-1</button>
+//                 <button onClick={this.resetAll}>Reset</button>
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(< Contador />, document.getElementById('root'));
+
+class Hidenshow extends React.Component {
+    constructor(props)
+
+    changeHidenshow = () => {
+        if (this.state.visibilidade === true){
+
+        } else {
+            this.setState(() => {
+                return {
+                    visibilidade:true
+                }
+            })
+        }
     }
 
     render() {
         return (
-            <div>
-                <h2>Count: {count}</h2>
-                <button onClick={this.addUm}>+1</button>
-                <button onClick={this.subtrairUm}>-1</button>
-                <button onClick={this.resetAll}>Reset</button>
-            </div>
-        )
-    }
+        <div>
+            
+            <h1>Hide 'n Show!</h1>
+            <button onClick={}>{this.state.visibilidade ? Esconder visibilidade : mostrar visibilidade}</button>
+            
+            <p>{ghost}</p>
+        </div>
+    )}
 }
 
-ReactDOM.render(< Contador />, document.getElementById('root'));
+ReactDOM.render(< Hidenshow />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ react/react-iv/Semana-16-Turma-7-master/projetinho/src/pages/Home/Home.js
@@ -0,0 +1,76 @@
+import React, { Component, Fragment } from 'react';
+import Cabecalho from '../../components/Cabecalho'
+import NavMenu from '../../components/NavMenu'
+import Dashboard from '../../components/Dashboard'
+import Widget from '../../components/Widget'
+import TrendsArea from '../../components/TrendsArea'
+import Tweet from '../../components/Tweet'
+
+class Home extends Component {
+  constructor(){
+    super()
+    this.state = {
+      novoTweet: "",
+      tweets:[],
+    }
+  }
+  adicionaTweet = (event) => {
+    event.preventDefault();
+    this.setState(stateAnterior => ({
+      tweets:[
+        /*spreads os tres tracinhos */
+        stateAnterior.novoTweet, ...stateAnterior.tweets
+      ], 
+      novoTweet: ''
+    }))
+    
+  }
+  render() {
+    console.log(this.state.tweets)
+    return (
+      <Fragment>
+        <Cabecalho>
+            <NavMenu usuario="@robertacintra" />
+        </Cabecalho>
+        <div className="container">
+            <Dashboard>
+                <Widget>
+                    <form className="novoTweet" onSubmit={this.adicionaTweet}>
+                        <div className="novoTweet__editorArea">
+                            <span className={`novoTweet__status ${this.state.novoTweet.length > 140 ? "novoTweet__status--invalido": "novoTweet__status" }`}>{this.state.novoTweet.length} /140</span>
+                            <textarea className="novoTweet__editor" value={this.state.novoTweet} placeholder="O que está acontecendo?" onChange ={(event) => this.setState({novoTweet: event.target.value})}></textarea>
+                        </div>
+                        <button type="submit" className = "novoTweet__envia" disabled ={this.state.novoTweet.length >140 }> Tweetar</button>
+                    </form>
+                </Widget>
+                <Widget>
+                    <TrendsArea />
+                </Widget>
+            </Dashboard>
+            <Dashboard posicao="centro">
+                <Widget>
+                    <div className="tweetsArea">
+                      {
+                        this.state.tweets.length > 0 ?
+                        this.state.tweets.map((elemento, index) => {
+                          return <Tweet texto={elemento} key={index}/>
+                        }) : 
+                        <p>Compartilhe seu primeiro tweet.</p>
+                      }
+                        {/* {
+                          this.state.tweets.length >0 ?
+                          this.state.tweets.map((elemento, index) => (
+                            <Tweet texto={elemento} key={index}/>
+                          )) :
+                          <p>Compartilhe seu primeiro tweet.</p>
+                        } */}
+                    </div>
+                </Widget>
+            </Dashboard>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Home;
